c52727d05fd3ead3de6da3206a7fae1d592ab5b2
Clare West <clare.west@dtc.ox.ac.uk>
2020-07-20 17:40:51 +0100

    Slow down carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -154,6 +154,7 @@
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
+    autoplayTimeout:15000,
     dots: true,
     loop: true,
     items: 1
